92ec59203e95333f1a5548acfc93713c10c76c61
ArishaMays1 <1arishamays1@gmail.com>
2023-06-27 23:01:54 -0400

    build.bat amendments and patches disabled

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,10 +2,11 @@
 
 :: We set CMAKE_DISABLE_FIND_PACKAGE_Protobuf=ON as currently we do not want to use
 :: protobuf from conda-forge, see https://github.com/conda-forge/onnxruntime-feedstock/issues/57#issuecomment-1518033552
-python tools/ci_build/build.py ^
+%python% tools/ci_build/build.py ^
+    --allow_running_as_root ^
     --compile_no_warning_as_error ^
     --build_dir build-ci ^
-    --cmake_extra_defines EIGEN_MPL2_ONLY=ON "onnxruntime_USE_COREML=OFF" "onnxruntime_BUILD_SHARED_LIB=ON" "onnxruntime_BUILD_UNIT_TESTS=ON" CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% CMAKE_DISABLE_FIND_PACKAGE_Protobuf=ON ^
+    --cmake_extra_defines EIGEN_MPL2_ONLY=ON "onnxruntime_USE_COREML=OFF" "onnxruntime_BUILD_SHARED_LIB=ON" "onnxruntime_BUILD_UNIT_TESTS=OFF" CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% CMAKE_DISABLE_FIND_PACKAGE_Protobuf=ON ^
     --cmake_generator Ninja ^
     --build_wheel ^
     --config Release ^
@@ -14,10 +15,7 @@ python tools/ci_build/build.py ^
     --skip_submodule_sync
 if errorlevel 1 exit 1
 
-python tools/ci_build/build.py --test  --config Release --cmake_generator Ninja --build_dir build-ci
-if errorlevel 1 exit 1
-
 :: In theory there should be only one wheel
 for %%F in (build-ci\Release\dist\onnxruntime-*.whl) do (
-    python -m pip install %%F
+    %python% -m pip install %%F
     if errorlevel 1 exit 1
