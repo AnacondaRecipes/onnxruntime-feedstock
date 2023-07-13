7157f325d636d4f8bfa1b2b8ef89fc02c15a6731
Arisha Mays <99516928+Arishamays1@users.noreply.github.com>
2023-07-13 17:25:36 -0400

    Update recipe/bld.bat
    Co-authored-by: Serhii Kupriienko <79282962+skupr-anaconda@users.noreply.github.com>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,7 @@
 
 :: We set CMAKE_DISABLE_FIND_PACKAGE_Protobuf=ON as currently we do not want to use
 :: protobuf from conda-forge, see https://github.com/conda-forge/onnxruntime-feedstock/issues/57#issuecomment-1518033552
-%python% tools/ci_build/build.py ^
+%PYTHON% tools/ci_build/build.py ^
     --compile_no_warning_as_error ^
     --build_dir build-ci ^
     --cmake_extra_defines EIGEN_MPL2_ONLY=ON "onnxruntime_USE_COREML=OFF" "onnxruntime_BUILD_SHARED_LIB=ON" "onnxruntime_BUILD_UNIT_TESTS=OFF" CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% CMAKE_DISABLE_FIND_PACKAGE_Protobuf=ON ^
