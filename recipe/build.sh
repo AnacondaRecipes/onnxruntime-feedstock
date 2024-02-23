eae36bfd6d5ea4949587d87a91fc40b3fd7e8fc3
Daniel Petry <dpetry@anaconda.com>
2024-02-23 15:15:42 -0600

    Specify CUDA architectures explicitly

--- recipe/build.sh
+++ recipe/build.sh
@@ -20,7 +20,8 @@ cmake_extra_defines=( "EIGEN_MPL2_ONLY=ON" \
                       "onnxruntime_DONT_VECTORIZE=$DONT_VECTORIZE" \
                       "onnxruntime_BUILD_SHARED_LIB=ON" \
                       "onnxruntime_BUILD_UNIT_TESTS=ON" \
-                      "CMAKE_PREFIX_PATH=$PREFIX"
+                      "CMAKE_PREFIX_PATH=$PREFIX" \
+                      "CMAKE_CUDA_ARCHITECTURES=all-major"
 		    )
 
 # Copy the defines from the "activate" script (e.g. activate-gcc_linux-aarch64.sh)
