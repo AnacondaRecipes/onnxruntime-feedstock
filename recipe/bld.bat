c4d5c5a87e34cd11fa53d60607289eaef1787600
Brian Wing <bwing@anaconda.com>
2025-01-30 15:58:43 -0500

    Updated to 1.20.1, refresh cuda build config
    Co-authored-by: skupr-anaconda <79282962+skupr-anaconda@users.noreply.github.com>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,18 +1,32 @@
 @echo on
 
+set cmake_extra_defines="EIGEN_MPL2_ONLY=ON onnxruntime_USE_COREML=OFF onnxruntime_BUILD_SHARED_LIB=ON onnxruntime_BUILD_UNIT_TESTS=ON CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% CMAKE_DISABLE_FIND_PACKAGE_Protobuf=ON"
+
+:: Enable CUDA support
+if "%ep_variant%" == "cuda" (
+    set "CUDAHOSTCXX=%CXX%"
+    set "cmake_extra_defines=%cmake_extra_defines% CMAKE_CUDA_COMPILER=%LIBRARY_BIN:\=/%/nvcc.exe"
+    set "CUDA_ARGS=--use_cuda --cuda_home %LIBRARY_PREFIX:\=/% --cudnn_home %LIBRARY_PREFIX% --enable_cuda_profiling"
+) else (
+    set "CUDA_ARGS="
+)
+
 :: We set CMAKE_DISABLE_FIND_PACKAGE_Protobuf=ON as currently we do not want to use
 :: protobuf from conda-forge, see https://github.com/conda-forge/onnxruntime-feedstock/issues/57#issuecomment-1518033552
 %PYTHON% tools/ci_build/build.py ^
     --compile_no_warning_as_error ^
+    --enable_pybind ^
     --build_dir build-ci ^
-    --cmake_extra_defines EIGEN_MPL2_ONLY=ON "onnxruntime_USE_COREML=OFF" "onnxruntime_BUILD_SHARED_LIB=ON" "onnxruntime_BUILD_UNIT_TESTS=ON" CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% CMAKE_DISABLE_FIND_PACKAGE_Protobuf=ON ^
+    --cmake_extra_defines %cmake_extra_defines% ^
     --cmake_generator Ninja ^
     --build_wheel ^
     --config Release ^
     --update ^
     --build ^
+    --parallel ^
     --test ^
-    --skip_submodule_sync
+    --skip_submodule_sync ^
+    %CUDA_ARGS%
 if errorlevel 1 exit 1
 
 :: In theory there should be only one wheel
